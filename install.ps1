b574a19bccc857e521c60e65d657a5123278c601
Sjoerd Wouters <sjoerdwouters2004@gmail.com>
2026-06-03 00:03:16 +0200

    stuff

--- install.ps1
+++ install.ps1
@@ -1,13 +1,7 @@
 # install the extension
 
-# install vsce globally (safe to re-run)
-npm install @vscode/vsce -g
-
-# package into vsix file
 vsce package
-
-# find vsix file
 $vsix = Get-ChildItem -Filter "*.vsix"
 
-# install vsix file
-code --install-extension $vsix.FullName
+code --uninstall-extension sjoerdev.jai-syntax
+code --install-extension $vsix.FullName --force
